6b2b7119f66c38d08d14984f05a3c367c978f60b
root <illjazz@gmx.de>
2018-05-25 12:14:45 +0000

    ;

--- www/index.php
+++ www/index.php
@@ -1,8 +1,8 @@
 <?php 
-echo '<h1>Host: '
+echo '<h1>Host: ';
 echo gethostname();
 echo '</h1>';
 $pwd = file_get_contents('/run/secrets/mysqlpwd');
-echo '<h2>Passwort: '
+echo '<h2>Passwort: ';
 echo $pwd;
 echo '</h2>';
